c4f3da9ff056f615b9fdd04af7f03e3e6410dcd2
George Alster <26443701+Alster96@users.noreply.github.com>
2019-05-02 15:31:49 -0400

    add and update files

---
+++ global.R
@@ -0,0 +1,8 @@
+## global.R ##
+
+# read csv file into a dataframe 
+crash_df = read.csv(file = 'data.csv', stringsAsFactors = FALSE)
+head(crash_df)
+# create two variables with Operator and Type column values as choice
+#choice1
+#choice2
---
+++ ui.R
@@ -0,0 +1,25 @@
+library(shinydashboard)
+shinyUI(dashboardPage(
+  dashboardHeader(title = "Air Crash Investigation"),
+  dashboardSidebar(
+    sidebarUserPanel(img(src='plane.jpeg',style="width: 100px")),
+    sidebarMenu(
+      menuItem("Interactive Globe ", tabName = "globe", icon = icon("map")),
+      menuItem("Annual Analysis", tabName = "annual", icon = icon("database")),
+      menuItem("Monthly Analysis", tabName = "monthly", icon = icon("database"))
+    )#,
+    #selectizeInput("selected","Select Operator",choice1),
+    #selectizeInput("selected","Select Aircraft Type",choice2)
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "globe",
+"to be replaced with interactive globe"),
+      tabItem(tabName = "annual", 
+"to be replaced with scatterplot showing across the years the highest fatality accidents"),
+tabItem(tabName = "monthly", 
+        "to be replaced with bar chart of count of accidents in each month")
+    )
+  )
+))
+
